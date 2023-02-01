56805a9a7d2001cbf19a057c7587c51fae6af506
Callum <callum.addison@hotmail.com>
2023-02-01 21:33:28 +0000

    Changed resoluton to 1366 x 768 for homepage window (default size for all?)
    Setup Label on homepage for the title

--- Homepage.Designer.vb
+++ Homepage.Designer.vb
@@ -24,32 +24,48 @@ Partial Class Homepage
     Private Sub InitializeComponent()
         Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Homepage))
         Me.btnHome = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
         Me.SuspendLayout()
         '
         'btnHome
         '
         Me.btnHome.Font = New System.Drawing.Font("Segoe UI", 12.0!, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point)
         Me.btnHome.Image = CType(resources.GetObject("btnHome.Image"), System.Drawing.Image)
-        Me.btnHome.Location = New System.Drawing.Point(708, 12)
+        Me.btnHome.Location = New System.Drawing.Point(1258, 12)
         Me.btnHome.Name = "btnHome"
         Me.btnHome.Size = New System.Drawing.Size(80, 77)
         Me.btnHome.TabIndex = 0
         Me.btnHome.TextAlign = System.Drawing.ContentAlignment.TopCenter
         Me.btnHome.UseVisualStyleBackColor = True
         '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Bahnschrift Condensed", 48.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.Label1.ForeColor = System.Drawing.SystemColors.ButtonHighlight
+        Me.Label1.Location = New System.Drawing.Point(311, 12)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(691, 77)
+        Me.Label1.TabIndex = 1
+        Me.Label1.Text = "YGS - Progression Step Monitor"
+        '
         'Homepage
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 15.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.BackColor = System.Drawing.SystemColors.ActiveBorder
-        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.ClientSize = New System.Drawing.Size(1350, 729)
+        Me.Controls.Add(Me.Label1)
         Me.Controls.Add(Me.btnHome)
+        Me.Font = New System.Drawing.Font("Segoe UI", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
         Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
         Me.Name = "Homepage"
         Me.Text = "Ysgol Greenhill School - Progress Step Monitor"
         Me.ResumeLayout(False)
+        Me.PerformLayout()
 
     End Sub
 
     Friend WithEvents btnHome As Button
+    Friend WithEvents Label1 As Label
 End Class
